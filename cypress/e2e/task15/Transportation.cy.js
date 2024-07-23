744242ac986172612a71ebc20d99cac313f66346
StefanVieriu <vieriucristianstefan1997@gmail.com>
2024-07-23 15:36:44 +0300

    Update Task 15, update cleaning code

--- cypress/e2e/task15/Transportation.cy.js
+++ cypress/e2e/task15/Transportation.cy.js
@@ -1,23 +1,25 @@
 describe('Validate Transportation Data for Location Vaslui', function () {
     const endpoint = 'https://api.amprenta.at.assistcloud.services/api/v1/transportations?location=Vaslui';
-    it('should have a status code of 201', function () {
+    let responseData;
+    beforeEach(function () {
         cy.request('POST', endpoint).then((response) => {
             expect(response.status).to.eq(201);
+            responseData = response.body;
         });
     });
+    it('should have a status code of 201', function () {
+        expect(responseData).to.exist; 
+    });
     it('should validate the response JSON format', function () {
-        cy.request('POST', endpoint).then((response) => {
-            const responseBody = response.body;
-            const expectedStructure = {
-                "cars_carbon_footprint": "number",
-                "flights_carbon_footprint": "number",
-                "public_transports_carbon_footprint": "number",
-                "footprint_id": "number"
-            };
-            Object.keys(expectedStructure).forEach((key) => {
-                expect(responseBody).to.have.property(key);
-                expect(responseBody[key]).to.be.a(expectedStructure[key]);
-            });
+        const expectedStructure = {
+            "cars_carbon_footprint": "number",
+            "flights_carbon_footprint": "number",
+            "public_transports_carbon_footprint": "number",
+            "footprint_id": "number"
+        };
+        Object.keys(expectedStructure).forEach((key) => {
+            expect(responseData).to.have.property(key);
+            expect(responseData[key]).to.be.a(expectedStructure[key]);
         });
     });
 });
